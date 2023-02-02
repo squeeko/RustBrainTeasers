db27e8aa3fe7e533d9a1b29227e5e890d74492c9
squeeko <squeeko@users.noreply.github.com>
2023-02-02 15:10:33 -0800

    Eureka! out-of-order

--- please-reboot-the-universe/src/main.rs
+++ please-reboot-the-universe/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    if 0.1 + 0.2 == 0.3 {
+        println!("Arithmetic still works!")
+    } else {
+        println!("Please reboot the universe!")
+    }
 }
+
+/*
+Again this is due to floats and how they compare. Rule of thumb, never compare floats or if so then use
+the floor() or ceil() methods. See the "three-and-a-bit" package.
+
+https://github.com/mikedilger/float-cmp
+Float Comparison Warning from Clippy
+https://rust-lang.github.io/rust-clippy/master/index.html#float_cmp
+*/
